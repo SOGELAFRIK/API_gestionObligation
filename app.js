cbeed2990a5fa64c77de7809249217804fdb992a
henoceli3 <afriksogel@gmail.com>
2023-04-29 22:43:31 +0000

    crud terminé

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ app.get('/', (req, res) => {
 
 //ici nos future points de terminaison
 
+/* CONSOLIDATION */
+require('./src/routes/consolidation/createConsolidation')(app);
+require('./src/routes/consolidation/deleteConsolidation')(app);
+require('./src/routes/consolidation/findAllConsolidation')(app);
+require('./src/routes/consolidation/findConsolidationById')(app);
+require('./src/routes/consolidation/updateConsolidation')(app);
+
 /* ENTITE */
 require('./src/routes/entite/createEntite')(app)
 require('./src/routes/entite/deleteEntite')(app)
@@ -30,7 +37,20 @@ require('./src/routes/obligation/deleteObligation')(app)
 require('./src/routes/obligation/finfAllObligation')(app)
 require('./src/routes/obligation/findBobligationById')(app)
 require('./src/routes/obligation/updtaeObligation')(app)
+
+/* ROLE */
+require('./src/routes/role/createRole')(app)
+require('./src/routes/role/deleteRole')(app)
+require('./src/routes/role/findAllRole')(app)
+require('./src/routes/role/findRoleByRole')(app)
+require('./src/routes/role/updateRole')(app);
+
 /* SUIVI OBLIGATION */
+require('./src/routes/suivi_obligation/createSuiviObligation')(app);
+require('./src/routes/suivi_obligation/deleteSuiviObligation')(app);
+require('./src/routes/suivi_obligation/findAllSuiviObligation')(app);
+require('./src/routes/suivi_obligation/findSuiviObligationById')(app);
+require('./src/routes/suivi_obligation/updateSuiviObligation')(app);
 
 /* UTILISATEUR */
 require('./src/routes/user/createUser')(app)
@@ -39,16 +59,17 @@ require('./src/routes/user/updateUser')(app)
 require('./src/routes/user/deleteUser')(app)
 require('./src/routes/user/findAllUser')(app)
 
-/* ROLE */
-require('./src/routes/role/createRole')(app)
-require('./src/routes/role/deleteRole')(app)
-require('./src/routes/role/findAllRole')(app)
-require('./src/routes/role/findRoleByRole')(app)
-require('./src/routes/role/updateRole')(app)
+
+
 /* WORKFLOW */
+require('./src/routes/workflow/createWorkflow')(app);
+require('./src/routes/workflow/updateWorkflow')(app);
+require('./src/routes/workflow/deleteWorkflow')(app)
+require('./src/routes/workflow/findAllWorkflow')(app);
+require('./src/routes/workflow/findWorkflowById')(app)
 
 /* LOGIN */
-require('./src/routes/login')(app)
+require('./src/routes/login')(app);
 
 app.use(({ res }) => {
     const message = "Impossible de trouver la ressource demander! vous pouver essayer une autre URL"
